e3de0eb5dc005c77018c23d20252f37e4b6ef37f
mohamedElsherkawyy <mohamedmotaz942@gmail.com>
2025-12-17 23:49:50 +0200

    Refactor llm.py to remove dotenv dependency, streamline STTRequest and STTResponse models, and enhance response parsing logic

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")
+if not groq_api_key:
+    raise ValueError("GROQ_API_KEY not found in environment variables")
--- llm.py
+++ llm.py
@@ -1,16 +1,8 @@
-import os
-from dotenv import load_dotenv
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel , Field
-from typing import List, Union, Optional
 from langchain_groq import ChatGroq
 from langchain_core.output_parsers import PydanticOutputParser 
-
-load_dotenv()
-
-groq_api_key = os.getenv("GROQ_API_KEY")
-if not groq_api_key:
-    raise ValueError("GROQ_API_KEY not found in environment variables")
+from config import groq_api_key
+from models import STTRequest, STTResponse
 
 llm = ChatGroq(
     model="llama-3.3-70b-versatile",
@@ -18,24 +10,10 @@ llm = ChatGroq(
     api_key=groq_api_key
 )
 
-class STTRequest(BaseModel):
-    stt_text: str
-
-class category(BaseModel):
-    food: Optional[str] = Field(default=None, description="The amount of money spent on food , drinks , coffee shops , restaurants , etc")
-    transportation: Optional[str] = Field(default=None, description="The amount of money spent on transportation , taxi , bus , metro , etc")
-    travel: Optional[str] = Field(default=None, description="The amount of money spent on travel , hotel , airbnb , etc")
-    shopping: Optional[str] = Field(default=None, description="The amount of money spent on shopping , clothes , shoes , etc , not including food and drinks")
-    fitness: Optional[str] = Field(default=None, description="The amount of money spent on fitness , gym , yoga , etc")
-    entertainment: Optional[str] = Field(default=None, description="The amount of money spent on entertainment , movies , concerts , games , etc")
-
-
-class STTResponse(BaseModel):
-    category: list[category]
+response_parser = PydanticOutputParser(pydantic_object=STTResponse)
+format_instructions = response_parser.get_format_instructions()
 
 def process_stt(request: STTRequest ):
-    response_parser = PydanticOutputParser(pydantic_object=STTResponse)
-    format_instructions = response_parser.get_format_instructions()
     prompt = f"""
         You are a specialized assistant that processes raw speech-to-text outputs and ocr outputs and organizes them into categories.
         The text in egyptian arabic.
@@ -53,7 +31,7 @@ def process_stt(request: STTRequest ):
         - if the text have Subtotal and Total take Total.
         """
     try:
-        response = llm.invoke(prompt + format_instructions)
+        response = llm.invoke(prompt)
         return {"response": response_parser.parse(response.content)}
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": str(e)})
